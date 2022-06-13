f3afd38bcb86f8f2dd83d968d9c1b299f0dbe25d
Sheyla de Araújo Barrientos <sheyla@jeraapps.com>
2022-06-13 17:23:19 -0300

    API Retrofit

---
+++ app/src/main/java/com/example/spectacle/data/Network.kt
@@ -0,0 +1,42 @@
+package com.example.spectacle.data
+
+import com.example.spectacle.data.remotesource.MoviesRemoteSource
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Network {
+    fun getMoviesRemoteSource(): MoviesRemoteSource {
+        val api = getService()
+        return api.create(MoviesRemoteSource::class.java)
+    }
+
+    private fun getService(): Retrofit {
+
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        val okHttpClient = OkHttpClient.Builder()
+        okHttpClient.addInterceptor(logging)
+        okHttpClient.addInterceptor { chain ->
+            val original = chain.request()
+            val originalHttpUrl = original.url
+
+            val url = originalHttpUrl.newBuilder()
+                .addQueryParameter("api_key", Constants.PRIVATE_KEY.value)
+                .addQueryParameter("language", "pt-BR")
+                .build()
+
+            chain.proceed(original.newBuilder().url(url).build())
+        }
+
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL.value)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient.build())
+            .build()
+    }
+}
