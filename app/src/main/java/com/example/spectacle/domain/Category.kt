ef401e8154273020d274d4edf972db3af9a52d38
Sheyla de Araújo Barrientos <sheyla@jeraapps.com>
2022-06-15 06:44:17 -0300

    Clean and Refactor

--- app/src/main/java/com/example/spectacle/domain/Category.kt
+++ app/src/main/java/com/example/spectacle/domain/Category.kt
@@ -1,10 +1,6 @@
 package com.example.spectacle.domain
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class Category(
     val id: Int,
     val name: String
-) : Parcelable
+)
